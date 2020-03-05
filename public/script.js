1b6d572cd005b54281f9b2867a399f6cf6227437
John Muir <johnmuir1907@gmail.com>
2020-03-05 17:26:55 +0000

    updated weather animation

--- public/script.js
+++ public/script.js
@@ -5,6 +5,11 @@ const input = document.getElementById("location")
 const city = document.getElementById("city")
 const wrap = document.getElementById("wrap")
 
+const sun = document.getElementById("sun")
+const cloudWrap = document.getElementById("cloudWrap")
+const cloudWrap2 = document.getElementById("cloudWrap2")
+const rain = document.getElementById("rainWrap")
+
 const cloudTitle = document.getElementById("cloudTitle")
 const cloud = document.getElementById("cloud")
 const tempTitle = document.getElementById("tempTitle")
@@ -18,26 +23,27 @@ const sky = () => {
     var today = new Date().getHours();
 
     if(today > 0 && today < 5){
-        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(1,4,27,1) 12%, rgba(0,20,113,1) 100%);"
-        wrap.style.backgroundColor = "rgb(0, 50, 130)"
-    } else if(today > 5 && today < 7){
-        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(255,231,145,1) 14%, rgba(255,157,76,1) 100%);"
-        wrap.style.backgroundColor = "rgb(255, 148, 61)"
-    } else if(today > 7 && today < 9){
-        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(255,212,123,1) 20%, rgba(76,144,255,1) 100%);"
-        wrap.style.backgroundColor = "rgb(255,212,123)"
-    } else if(today > 9 && today < 18){
-        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(125, 226, 255,1) 0%, rgba(194, 242, 255,1) 72%)"
-        wrap.style.backgroundColor = "rgb(110, 202, 255)"
-    } else if(today > 18 && today < 20){
-        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(255,212,123,1) 19%, rgba(183,121,247,1) 52%, rgba(76,144,255,1) 100%);"
-        wrap.style.backgroundColor = "rgb(175, 170, 250)"
-    } else if(today > 20 && today < 24){
-        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(1,4,27,1) 12%, rgba(0,20,113,1) 100%);"
-        wrap.style.backgroundColor = "rgb(0, 50, 130)"
+        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(1,4,27,1) 12%, rgba(0,20,113,1) 100%)";
+        wrap.style.setProperty('--card-color', 'rgb(0, 50, 130)');
+        document.documentElement.style.setProperty('--sun-color', '#f0faff');
+    } else if(today >= 5 && today < 9){
+        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(255,212,123,1) 20%, rgba(76,144,255,1) 100%)";
+        wrap.style.setProperty('--card-color', 'rgb(107, 163, 255)');
+    } else if(today >= 9 && today < 18){
+        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(125, 226, 255,1) 0%, rgba(194, 242, 255,1) 72%)";
+        wrap.style.setProperty('--card-color', 'rgb(136, 211, 255)');
+    } else if(today >= 18 && today < 20){
+        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(255,212,123,1) 19%, rgba(183,121,247,1) 52%, rgba(76,144,255,1) 100%)";
+        wrap.style.setProperty('--card-color', 'rgb(150, 197, 255)');
+    } else if(today >=   20 && today < 24){
+        document.body.style.backgroundImage = "linear-gradient(0deg, rgba(1,4,27,1) 12%, rgba(0,20,113,1) 100%)";
+        wrap.style.setProperty('--card-color', 'rgb(0, 50, 130)');
+        document.documentElement.style.setProperty('--sun-color', '#f0faff');
 }
 }
+
 sky()
+
 
 btn.addEventListener('click', async () => {
     const response = await fetch(`/weather?address=${input.value}`)
@@ -46,6 +52,7 @@ btn.addEventListener('click', async () => {
         console.log(result.error)
         city.textContent = result.error
     } else {
+        console.log(result)
         city.textContent = result.location
         cloudTitle.textContent = "Cloud Coverage:"
         cloud.textContent = result.data.summary
@@ -53,6 +60,48 @@ btn.addEventListener('click', async () => {
         temp.textContent = `${((result.data.temperature - 32)*5/9).toFixed(2)}°C`
         windTitle.textContent = "Wind Speed:"
         wind.textContent = `${result.data.windSpeed}mph`
-        console.log(result)
+        if(result.data.icon == "clear-day"){
+            sun.style.display = "block"
+            cloudWrap.style.display = "none"
+            rain.style.display = "none"
+        } else if(result.data.icon == "clear-night"){
+            sun.style.display = "block"
+            cloudWrap.style.display = "none"
+            rain.style.display = "none"
+            document.documentElement.style.setProperty('--sun-color', '#ffd058 ');
+        } else if(result.data.icon == "rain" || result.data.icon == "sleet"){
+            document.documentElement.style.setProperty('--cloud-color', '#ababab');
+            sun.style.display = "none"
+            cloudWrap.style.display = "block"
+            rain.style.display = "block"
+            document.documentElement.style.setProperty('--sun-color', '#ffd058 ');
+        } else if(result.data.icon == "fog"){
+            sun.style.display = "none"
+            cloudWrap.style.display = "block"
+            cloudWrap2.style.display = "block"
+            rain.style.display = "none"
+            document.documentElement.style.setProperty('--sun-color', '#ffd058 ');
+        } else if(result.data.icon == "cloudy"){
+            sun.style.display = "none"
+            cloudWrap.style.display = "block"
+            cloudWrap2.style.display = "block"
+            document.documentElement.style.setProperty('--cloud-color', '#ababab');
+            rain.style.display = "none"
+            document.documentElement.style.setProperty('--sun-color', '#ffd058 ');
+        } else if(result.data.icon == "partly-cloudy-day"){
+            sun.style.display = "block"
+            cloudWrap.style.display = "block"
+            cloudWrap2.style.display = "none"
+            document.documentElement.style.setProperty('--cloud-color', '#ffffff');
+            rain.style.display = "none"
+            document.documentElement.style.setProperty('--sun-color', '#ffd058 ');
+        } else if(result.data.icon == "partly-cloudy-night"){
+            sun.style.display = "block"
+            cloudWrap.style.display = "block"
+            cloudWrap2.style.display = "none"
+            document.documentElement.style.setProperty('--cloud-color', '#ffffff');
+            rain.style.display = "none"
+            document.documentElement.style.setProperty('--sun-color', '#f0faff');
+        }
     }
 })
